5896359b5e952de3d6a9ba5d68573952685c146d
timefliez1210 <crfabig@gmail.com>
2020-05-06 17:21:54 +0800

    vorläufiger fix

---
+++ pages/blog/post.js
@@ -0,0 +1,340 @@
+import Head from "next/head";
+import Link from "next/link";
+
+const Post = () => (
+  <div>
+    <Head>
+      <title>Aust Umzüge und Haushaltsauflösungen</title>
+    </Head>
+    <div className="seperator">
+      <h3>
+        <Link href="/kostenloses-angebot">
+          <a title="Zum kostenlosen Angebot">
+            <img
+              src="/static/img/message-icon.png"
+              alt="Schreiben Sie uns eine Nachricht"
+            />
+            Kostenloses Angebot erhalten
+          </a>
+        </Link>
+      </h3>
+    </div>
+    <div class="wrapper">
+      <h2>4 Wochen vor dem Umzug</h2>
+      <br />
+      <br />
+      <h3>Telefon und Internet kündigen oder ummelden</h3>
+      <br />
+      <br />
+      <p>
+        Stellen Sie Ihre Erreichbarkeit in der neuen Wohnung sicher und kümmern
+        Sie sich rechtzeitig um die Ummeldung von Telefon und Internet. Da die
+        Umstellung etwas länger dauern kann und dies oft nur mit dem
+        Monatswechsel möglich ist, sollten Sie das jetzt machen.
+      </p>
+      <br />
+      <br />
+      <h3>Stromanbieter / Gasanbieter kündigen oder ummelden</h3>
+      <br />
+      <br />
+      <p>
+        Melden Sie Ihren Wohnortwechsel bei Ihrem Stromanbieter an. Dieser
+        stellt dann alles mit dem nächsten Monatswechsel um. Bei einem Umzug
+        haben Sie zudem die Möglichkeit von Ihrem Sonderkündigungsrecht Gebrauch
+        zu machen. Anbieter vergleichen lohnt sich!
+      </p>
+      <br />
+      <br />
+      <h3>Rundfunkgebühren (GEZ) ummelden</h3>
+      <br />
+      <br />
+      <p>
+        Jeder Haushalt in Deutschland muss die Rundfunkgebühren (GEZ) bezahlen.
+        Bei einem Umzug können Sie die Ummeldung ganz bequem und kostenlos
+        online vornehmen. Kümmern Sie sich darum und ersparen Sie sich unnötige
+        und lästige Briefe.
+      </p>
+      <br />
+      <br />
+      <h3>Abonnements kündigen</h3>
+      <br />
+      <br />
+      <p>
+        Bestehende Abonnements am besten rechtzeitig vor dem Monatswechsel
+        kündigen. Zeitungsabonnements können Sie ummelden und erhalten alles wie
+        gewohnt an Ihrer neuen Anschrift.
+      </p>
+      <br />
+      <br />
+      <h3>Arbeitgeber informieren</h3>
+      <br />
+      <br />
+      <p>
+        Teilen Sie Ihrem Arbeitgeber Ihre neue Anschrift mit, dies ist nicht nur
+        für Ihre Gehaltsabrechnung elementar wichtig! In den meisten
+        Arbeitsverträgen ist dies auch schriftlich festgehalten.
+      </p>
+      <br />
+      <br />
+      <h3>Behörden und Ämter informieren</h3>
+      <br />
+      <br />
+      <p>
+        Nutzen Sie Umzug Checkliste für Behördengänge. Vergessen Sie das
+        Finanzamt nicht! Dies erfährt nicht automatisch von Ihrer
+        Adressänderung. Melden Sie dies und Ihre neue Steuernummer Ihrem alten
+        Finanzamt. Falls Sie Leistungen von Behörden beziehen, informieren Sie
+        die Stellen um Ausfälle oder Kürzungen zu vermeiden.
+      </p>
+      <br />
+      <br />
+      <h3>Vereinbaren Sie einen Termin zur Wohnungsübergabe (alt/neu)</h3>
+      <br />
+      <br />
+      <p>
+        Damit Sie schnell Ihre Mietkaution wieder bekommen und die alte Wohnung
+        ordnungsgemäß übergeben können, vereinbaren Sie einen Termin zur
+        Übergabe. Dies gilt auch für die neue Wohnung, so wissen Sie ganz genau,
+        wann Sie in die Wohnung können, um vielleicht schon mal zu malern.
+      </p>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <h2>3 Wochen vor dem Umzug</h2>
+      <br />
+      <br />
+      <h3>Umzugsmaterial beschaffen</h3>
+      <br />
+      <br />
+      <p>
+        Nachdem Sie aufgelistet haben, was alles mit umziehen soll, benötigen
+        Sie ausreichend Umzugsmaterial. Vergleichen Sie vorher die Preise der
+        Anbieter. Besorgen Sie sich ausreichend Umzugskartons, Kleiderboxen,
+        Polsterfolie, Paketband etc.
+      </p>
+      <br />
+      <br />
+      <h3>Schönheitsreparaturen in der alten Wohnung</h3>
+      <br />
+      <br />
+      <p>
+        Achten Sie auf Türen, Wände und Fußboden und nehmen Sie vor dem Auszug
+        Schönheitsreparaturen vor, bevor Ihnen die Mängel von der Mietkaution
+        abgezogen werden. Verschließen Sie bspw. Dübellöcher, säubern Sie die
+        Wohnung für eine besenreine Übergabe.
+      </p>
+      <br />
+      <br />
+      <h3>Kinderbetreuung für den Umzugstag organisieren</h3>
+      <br />
+      <br />
+      <p>
+        Fall Sie kleine Kinder in Ihrem Haushalt haben, ist es ratsam sich um
+        eine Kinderbetreuung am Umzugstag zu kümmern. Sie werden am Umzugstag
+        sehr eingespannt sein und keine Zeit haben, sich großartig um Ihre
+        Kleinsten zu kümmern.
+      </p>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <h2>2 Wochen vor dem Umzug</h2>
+      <br />
+      <br />
+      <h3>Renovierung der neuen Wohnung, falls notwendig</h3>
+      <br />
+      <br />
+      <p>
+        Falls die neue Wohnung unrenoviert übergeben wird und Sie schon einen
+        Schlüssel haben, können Sie nun mit den renovierungsarbeiten beginnen.
+        Streichen Sie die Wände in Ihre Wunschfarbe und sie haben nun genügend
+        Zeit zum Trocknen.
+      </p>
+      <br />
+      <br />
+      <h3>Möbeldemontage</h3>
+      <br />
+      <br />
+      <p>
+        Demontieren Sie große und sperrige Möbelstücke, die nicht in einem Stück
+        verladen und transportiert werden können. Unterschätzen Sie die
+        Küchendemontage dabei nicht, die nimmt viel Zeit in Anspruch. Holen Sie
+        sich am besten Handwerker dazu.
+      </p>
+      <br />
+      <br />
+      <h3>Einrichtung einer Halteverbotszone für den Umzugstag</h3>
+      <br />
+      <br />
+      <p>
+        Ersparen Sie sich die ewige Parkplatzsuche, lange Laufwege am Umzugstag
+        oder sogar Strafzettel, wegen falsch parken. Beantragen Sie eine
+        Halteverbotszone am Umzugstag. Dies tun Sie bei der Stadt. Am besten
+        gleich für beide Anschriften, falls ein Parkproblem herrscht.
+      </p>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <h2>1 Wochen vor dem Umzug</h2>
+      <br />
+      <br />
+      <h3>Gardinenstangen, Lampen, etc. demontieren</h3>
+      <br />
+      <br />
+      <p>
+        Nehmen Sie Gardinen und Gardinenstangen in der jetzigen Wohnung ab.
+        Deinstallieren Sie Lampen oder andere elektrische Geräte. Prüfen Sie
+        vorher, dass der Strom ausgeschaltet ist. Verspachteln Sie im Anschluss
+        alle Löcher in den Wänden und an der Decke.
+      </p>
+      <br />
+      <br />
+      <h3>Alles transportsicher verpacken</h3>
+      <br />
+      <br />
+      <p>
+        Machen Sie Ihr Umzugsgut transportsicher. Die Schränke sind leer
+        geräumt, nun sichern Sie Schubläden, Einsätze, Schranktüren etc., damit
+        diese nicht beim Transport aufgehen oder herausfallen. Einlegeböden
+        empfehlen wir deshalb herauszunehmen.
+      </p>
+      <br />
+      <br />
+      <h3>Einkäufe vor dem Umzug</h3>
+      <br />
+      <br />
+      <p>
+        Damit Ihnen am Umzugstag nichts fehlt sollten Sie jetzt noch einmal
+        einkaufen gehen. Prüfen Sie Reinigungsmittel, Einmalhandschuhe,
+        Küchenpapier und natürlich Verpflegung. Sollten Sie noch Umzugsmaterial
+        benötigen, ist dies die letzte Chance vor dem Umzugstag.
+      </p>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <h2>Direkt nach dem Umzug</h2>
+      <br />
+      <br />
+      <h3>Möbelmontage</h3>
+      <br />
+      <br />
+      <p>
+        Montieren Sie Ihre Möbel selber oder engagieren Sie Handwerker für den
+        Möbelaufbau und die Küchenmontage. Falls Sie mit Ihrer Küche umziehen
+        muss diese unter Umständen angepasst werden, hierzu sollten Sie
+        unbedingt Handwerker heranziehen.
+      </p>
+      <br />
+      <br />
+      <h3>Alles auspacken und verräumen</h3>
+      <br />
+      <br />
+      <p>
+        Packen Sie nun Ihre Umzugskartons aus und verräumen Sie alles wieder an
+        seinen Platz in die aufgebauten Schränke, die inzwischen ihren
+        Bestimmungsort gefunden haben. Wenn Sie Ihre Umzugskartons vorher
+        entsprechend beschriftet haben, geht das Auspacken sehr schnell.
+      </p>
+      <br />
+      <br />
+      <h3>Entsorgung von Umzugsmaterial und Verpackungsmaterial</h3>
+      <br />
+      <br />
+      <p>
+        Benutztes Umzugsmaterial und das Verpackungsmaterial von Ihren
+        bestellten Möbeln können Sie nun fachgerecht und vorschriftsmäßig
+        entsorgen. Insofern Sie eine Umzugsfirma beauftrag haben, wird dies auf
+        Wunsch auch von der beauftragten Firma übernommen.
+      </p>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+      <h2>1 Woche nach dem Umzug</h2>
+      <br />
+      <br />
+      <h3>Wohnsitz ummelden</h3>
+      <br />
+      <br />
+      <p>
+        In Deutschland gibt es eine Meldepflicht, die jeden dazu verpflichtet
+        seinen aktuellen Wohnsitz anzumelden. Dafür gibt es eine Meldefrist
+        (zwei Wochen), die allerdings je nach Bundesland unterschiedlich ist.
+        Die Ummeldung erfolgt beim Einwohnermeldeamt. Nach dem Umzug ummelden –
+        Checkliste prüfen!
+      </p>
+      <br />
+      <br />
+      <h3>KFZ-Ummeldung</h3>
+      <br />
+      <br />
+      <p>
+        Für die KFZ-Ummeldung gilt das Gleiche, wie für die Wohnsitzummeldung
+        nach einem Umzug. Wenden Sie sich an die KFZ-Zulassungsstelle. Ihre
+        Nummernschilder dürfen Sie jedoch behalten und müssen nach der neuen
+        Verordnung nicht mehr getauscht werden.
+      </p>
+      <br />
+      <br />
+      <h3>Hund ummelden</h3>
+      <br />
+      <br />
+      <p>
+        Auch Ihren geliebten Vierbeiner müssen Sie zeitnah nach Ihrem Umzug
+        ummelden. Sie zahlen Hundesteuer, daher muss das Finanzamt hier
+        informiert sein und Ihr Hund umgemeldet sein. Mit der Checkliste nach
+        dem Umzug ummelden nicht vergessen!
+      </p>
+      <br />
+      <br />
+      <h3>Adressänderung bekannt geben</h3>
+      <br />
+      <br />
+      <p>
+        Informieren Sie alle über Ihren Wohnsitzwechsel und geben Sie Ihre neue
+        Anschrift bekannt. Dies reicht in der Regel telefonisch oder per E-Mail.
+        Nur in wenigen Fällen benötigen Sie einen Wohnsitznachweis. Mit der
+        Umzug Checkliste für Adressänderung vergessen Sie dies garantiert nicht.
+      </p>
+      <br />
+      <br />
+      <h3>Nachsendeantrag bei der Post stellen</h3>
+      <br />
+      <br />
+      <p>
+        Mit einem Nachsendeantrag von der Post stellen Sie sicher, dass Sie Ihre
+        Post auch dann erhalten, falls Sie jemanden vergessen haben zu
+        informieren. Den Nachsendeantrag können Sie für 6, 12 oder 24 Monate bei
+        der Post stellen. Nicht vergessen, mit der Checkliste für Ihren Umzug –
+        privat und gewerblich!
+      </p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Link href="/ratgeber">
+        <a
+          class="button"
+          title="Zurück zu Leistungen für Umzüge und Haushaltsauflösungen"
+        >
+          <b>Zurück zur Übersicht</b>
+        </a>
+      </Link>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  </div>
+);
+export default Post;
--- pages/ratgeber.js
+++ pages/ratgeber.js
@@ -3,7 +3,7 @@ import Posts from "../components/blog-items/blog-items.component";
 import fetch from "isomorphic-unfetch";
 import Link from "next/link";
 
-const Ratgeber = (props) => (
+const Ratgeber = () => (
   <div>
     <Head>
       <title>Aust Umzüge und Haushaltsauflösungen</title>
@@ -28,21 +28,58 @@ const Ratgeber = (props) => (
       </h3>
     </div>
     <div className="wrapper">
-      <Posts {...props} />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h2>Welche Vorbereitung müssen Sie vor Ihrem Umzug beachten?</h2>
+      <br />
+      <br />
+      <h3>
+        Und woran sollten Sie auch <u>nach</u> Ihrem Umzug denken?
+      </h3>
+      <br />
+      <br />
+      <p>
+        In unserem ersten Beitrag rund um das Thema umziehen möchten wir Ihnen
+        eine nützliche Checkliste bereitstellen.
+        <br />
+        Bei jedem Umzug gibt es allerhand Dinge zu beachten und wir wollen Sie
+        vorsorglich Informieren, sodass Sie alles reibungslos Planen können und
+        vor allem nicht an unvorhergesehenen Problemen verzeifeln.
+        <br />
+        <br />
+        Lesen Sie also hier über typische Stolpersteine und lassen Sie uns Ihnen
+        kostenfrei dabei helfen, Ihren Umzug so entspant wie möglich zu
+        gestalten.
+        <br />
+        Selbstverständlich können Sie uns jederzeit auch persönlich um Rat
+        fragen.
+      </p>
+
+      <br />
+      <br />
+      <Link href="/blog/post">
+        <a title="Checkliste für Ihren Umzug" class="button">
+          Ihre Checkliste
+        </a>
+      </Link>
+      <br />
+      <br />
       <br />
       <br />
     </div>
   </div>
 );
 
-Ratgeber.getInitialProps = async function () {
-  const res = await fetch("https://my-awesom-blog-api-crf.herokuapp.com/alex/");
-  const data = await res.json();
+// Ratgeber.getInitialProps = async function () {
+//   const res = await fetch("https://my-awesom-blog-api-crf.herokuapp.com/alex/");
+//   const data = await res.json();
 
-  return {
-    posts: data,
-  };
-};
+//   return {
+//     posts: data,
+//   };
+// };
 
 export default Ratgeber;
 
