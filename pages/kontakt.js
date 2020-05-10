7a8b3f2e3b31788e09fd22dc823b61132e53d0bf
timefliez1210 <crfabig@gmail.com>
2020-05-10 10:55:21 +0800

    maybe fix

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -45,14 +45,13 @@ const Kontakt = () => (
         name="Kontaktformular"
         data-netlify="true"
       >
-        {/* <input type="hidden" name="form-name" value="contact" /> */}
+        <input type="hidden" name="form-name" value="contact" />
         <div className="name">
           <label for="name"></label>
           <input
             type="text"
             placeholder="Mein Name ist.."
             name="name1"
-            id="name_input"
             required
           />
         </div>
@@ -62,7 +61,6 @@ const Kontakt = () => (
             type="email"
             placeholder="Meine Email ist..."
             name="email1"
-            id="email_input"
             required
           />
         </div>
@@ -72,7 +70,6 @@ const Kontakt = () => (
             type="text"
             placeholder="Meine Nummer ist.."
             name="telephone1"
-            id="telephone_input"
             required
           />
         </div>
@@ -82,7 +79,6 @@ const Kontakt = () => (
           <textarea
             name="message1"
             placeholder="Ich würde gerne folgendes wissen.."
-            id="message_input"
             cols="30"
             rows="5"
             required
